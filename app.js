49b19f3b19d416ec45694919833001c2e877e5ec
Chen <jayden.chen@sap.com>
2018-08-20 16:46:43 -0400

    Remove Redis, added mongo and express

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const express = require('express');
+const app = express();
+// Connection URL
+const url = 'mongodb://localhost:27017';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Database Name
+const dbName = 'myproject';
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+app.get('/', function(req, res) {
+  res.send('Hello World')
+})
+
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+
+  client.close();
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
